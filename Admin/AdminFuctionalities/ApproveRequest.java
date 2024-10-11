5b6fb0dd5f80d442828889dbbb6470ed2c068566
era97 <afrinjahanera03@gmail.com>
2024-10-12 00:30:39 +0600

    added approve_request

---
+++ Admin/AdminFuctionalities/ApproveRequest.java
@@ -0,0 +1,24 @@
+package Admin.AdminFuctionalities;
+
+import java.util.Scanner;
+
+import Admin.AdminDashboard;
+
+public class  ApproveRequest{
+    public void approveTransportAgencyRequests(){
+
+        AdminDashboard adminDashboard=new AdminDashboard();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 if there is delete account request else enter 0");
+        int userInput = scanner.nextInt();
+
+        if(userInput==1){
+            System.out.println("Request Approved");
+        }
+        else{
+            System.out.println("Request not approved");
+        }
+        adminDashboard.displayAdminMenu();
+    }
+}
+
