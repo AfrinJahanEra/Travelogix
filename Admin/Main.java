23fcf00c80ced9bb2329e12f3872a7ee0a09177b
era97 <afrinjahanera03@gmail.com>
2024-10-11 22:45:57 +0600

    New file added

---
+++ Admin/Main.java
@@ -0,0 +1,11 @@
+package Admin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AdminDashboard adminDashboard = new AdminDashboard();
+        adminDashboard.displayAdminMenu();
+    }
+}
+
---
+++ Utilities_Package/CustomFileManager/CustomList.java
@@ -0,0 +1,50 @@
+package Utilities_Package.CustomFileManager;
+
+
+public class CustomList<T> {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public CustomList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    // add elements to the list
+    public void add(T element) {
+        if (size == elements.length) {
+            resizeArray();
+        }
+        elements[size++] = element;
+    }
+
+    // get an element by index
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) elements[index];
+    }
+
+    // update an element at a specific index
+    public void set(int index, T element) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        elements[index] = element;
+    }
+
+    // return the size of the list
+    public int size() {
+        return size;
+    }
+
+    // resize the array when capacity is reached
+    private void resizeArray() {
+        int newCapacity = elements.length * 2;
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        elements = newArray;
+    }
+}
