027a1e15338a90a988f1ad8afae5bacd69416965
Ridika Naznin <168357027+ridika-115@users.noreply.github.com>
2024-10-11 20:45:07 +0600

    Add files via upload

---
+++ Agency/Main.java
@@ -0,0 +1,10 @@
+package Agency;
+
+import Bus.Addbus;
+
+public class Main {
+    public static void main(String[] args) {
+        Agency agency = new Agency();
+        agency.agency();
+    }
+}
