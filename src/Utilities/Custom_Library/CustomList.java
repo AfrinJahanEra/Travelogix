943fcbf738b765c12485996f3469a7f91cc757b9
era97 <afrinjahanera03@gmail.com>
2024-10-12 01:27:04 +0600

    comments added

--- src/Utilities/Custom_Library/CustomList.java
+++ src/Utilities/Custom_Library/CustomList.java
@@ -1,6 +1,7 @@
 package src.Utilities.Custom_Library;
 
 public class CustomList<T> {
+    
     private Object[] elements;
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
