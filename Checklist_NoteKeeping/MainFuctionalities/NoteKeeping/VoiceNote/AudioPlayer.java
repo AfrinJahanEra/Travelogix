ad6bf90ac7aa590f2b2f487e367fcbcfd558ef8a
era97 <afrinjahanera03@gmail.com>
2024-10-13 13:45:55 +0600

    Remove Byte function

--- Checklist_NoteKeeping/MainFuctionalities/NoteKeeping/VoiceNote/AudioPlayer.java
+++ Checklist_NoteKeeping/MainFuctionalities/NoteKeeping/VoiceNote/AudioPlayer.java
@@ -17,12 +17,7 @@ public class AudioPlayer {
             audioLine.start();
             System.out.println("Playing audio...");
 
-            byte[] buffer = new byte[4096];
-            int bytesRead = -1;
-
-            while ((bytesRead = audioStream.read(buffer)) != -1) {
-                audioLine.write(buffer, 0, bytesRead);
-            }
+            
 
             audioLine.drain();
             audioLine.close();
