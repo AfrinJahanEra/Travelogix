efd5a0d8419047669282424b188bfc9a605b48f6
Ridika Naznin <168357027+ridika-115@users.noreply.github.com>
2024-11-03 10:49:48 +0600

    Add files via upload

---
+++ src/Transport/Seat/SeatBooking.java
@@ -0,0 +1,44 @@
+package Source.Seat;
+import Source.File.*;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SeatBooking {
+    private FileHandler fileHandler;
+    private Scanner sc;
+    private SeatManager seatManager;
+
+    public SeatBooking(String filePath) {
+        this.fileHandler = new FileHandler(filePath);
+        this.sc = new Scanner(System.in);
+        this.seatManager = new SeatManager();
+    }
+
+    public void initiateBooking() throws IOException {
+        System.out.print("Enter the number plate of the bus to book seats: ");
+        String numberPlate = sc.nextLine().trim();
+
+        String content = fileHandler.readFromFile();
+        String[] lines = content.split("\n");
+
+        String[] busDetails = findBusDetails(lines, numberPlate);
+        if (busDetails != null) {
+            seatManager.setupSeatManager(busDetails);
+            seatManager.bookSeats(sc);
+            fileHandler.writeToFile(seatManager.updateBusDetails(lines, busDetails));
+        } else {
+            System.out.println("Bus with number plate " + numberPlate + " not found.");
+        }
+    }
+
+    public String[] findBusDetails(String[] lines, String numberPlate) {
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts[4].trim().equalsIgnoreCase(numberPlate)) {
+                return parts;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/Transport/Seat/ShowSeat.java
@@ -0,0 +1,30 @@
+package Source.Seat;
+
+
+import java.util.ArrayList;
+
+public class ShowSeat {
+    public String displaySeatMatrix(int row, int col, ArrayList<String> bookedSeats) {
+        char[] columns = new char[col];
+        for (int i = 0; i < col; i++) {
+            columns[i] = (char) ('A' + i);
+        }
+        String seatline = "";
+        System.out.println("Seat matrix:");
+        for (int i = 1; i <= row; i++) {
+            String seatshow = "";
+            for (int j = 0; j < col; j++) {
+                String seat = i + "" + columns[j];
+                if (bookedSeats.contains(seat)) {
+                    seatshow+="[X] ";
+                } else {
+                    seatshow+="[" + seat + "] ";
+                }
+            }
+            seatline+=seatshow+"\n";
+            System.out.println(seatshow);
+        }
+
+        return seatline;
+    }
+}
