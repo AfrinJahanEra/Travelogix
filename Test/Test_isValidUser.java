1515e95469468ec16a21b7a4210ccea26884ccd1
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2024-11-04 23:08:52 +0600

    Added unit test for isValidUser method

--- Test/Test_isValidUser.java
+++ Test/Test_isValidUser.java
@@ -10,14 +10,14 @@ class Test_isValidUser {
     @Test
     public void test1(){
         Authentication a= new Authentication();
-        assertTrue(a.isValidUser("ramisa@gmail.com"));
+        assertTrue(a.isValidUser("ramisa@gmail.com", "25d55ad283aa400af464c76d713c07ad"));
 
     }
 
     @Test
     public void test2(){
         Authentication a= new Authentication();
-        assertTrue(a.isValidUser("rina@gmail.com"));
+        assertTrue(a.isValidUser("rina@gmail.com", "25d55ad283aa400af464c76d713c07ad"));
 
     }
 
